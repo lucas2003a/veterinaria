b06e55e2c5d32e1e549ff44515b5dbab0f7301bb
Lucas Atúncar <00139645@senati.pe>
2023-03-20 15:05:03 -0500

    sitio original de veterianaria

---
+++ js/scrips.js
@@ -0,0 +1,10 @@
+function saludar (){
+    alert("Hola querida mascota");
+}
+
+function presentarse(){
+    alert("te saluda Lucas");
+}
+
+    const botonsaludo = document.getElementById("saludo");
+    botonSaludo.addEventeListener("click",saludar);
